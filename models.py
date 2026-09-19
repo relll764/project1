ad4ce182d67a4943c81ee2ada4487be2bc1d518a
relll764 <rveliev790@gmail.com>
2026-09-20 03:12:06 +0400

    -

--- models.py
+++ models.py
@@ -3,16 +3,6 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
 
-class Task(Base):
-    __tablename__ = "tasks"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    is_done = Column(Boolean, default=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
